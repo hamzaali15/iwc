b383e0d8b6b995197cb5ca2b6c0f65544b76d0f0
hamzaali15 <malikhamzaali48@gmail.com>
2023-02-24 23:10:31 +0500

    updated

--- iwc/hook_events/timesheet.py
+++ iwc/hook_events/timesheet.py
@@ -12,4 +12,4 @@ def calculate_total_amounts(self, method):
 	for d in self.get("time_logs"):
 		self.ot_hours += flt(d.ot)
 		self.regular_hours += flt(d.hours)
-	self.total_hours += flt(self.ot_hours if self.ot_hours else 0 + self.regular_hours if self.regular_hours else 0)
+	self.total_hours += (self.ot_hours if self.ot_hours else 0) + (self.regular_hours if self.regular_hours else 0)
